e26974b32ef6de5b040f881012dec5eb684b6564
VLSI2_FS26 <vlsi2_42fs26>
2026-08-22 10:52:53 +0200

    filelist for jtag_march tb added

---
+++ verilator/jtag_march.f
@@ -0,0 +1,9 @@
++incdir+../rtl/common_cells/include
++incdir+../rtl/obi/include
+../rtl/march_bist_controller.sv
+../rtl/fifo.sv
+../rtl/RM_IHPSG13_1P_core_behavioral_bm_bist_fault_model.v 
+../rtl/jtag_tap_controller.sv
+../rtl/jtag_tap_top.sv
+../rtl/test/tb_jtag_march.sv
+
